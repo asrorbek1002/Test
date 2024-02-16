73974c94bd53fe914b5d8eb905f6c5aa6e287d7c
Asrorbek <151444045+asrorbek1002@users.noreply.github.com>
2024-02-16 20:55:15 +0500

    Wikipedia.py

--- Wikipedia.py
+++ Wikipedia.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, types, executor
 
 
 # TOKENni yozing
-API_TOKEN = ""
+API_TOKEN = "6492517406:AAFlHpq3IWMDZxkYb7GW7xE8yeJK2IEwq8M"
 
 wikipedia.set_lang("uz")
 # AIogram bot obyekti yaratish va Dispatcher(uqituvchi) yaratish
@@ -26,7 +26,7 @@ async def sendWiki(message: types.Message):
         	respond = wikipedia.summary(message.text)
         	await message.answer(respond)
         except:
-        	await message.answer("bu mavzuga oid maqola topilmadi boshqa narsa kiritib koʻring")
+        	await message.answer("So'rovingiz bo'icha malumot topilmadi")
         
 
 if __name__ == '__main__':
